018432319ae5b0842d7a210d3991886ddd7fe04e
hoangduong-coder <hoangduongphantri@gmail.com>
2022-02-02 18:02:41 +0200

    First commit

---
+++ part9/bmiCalculator.ts
@@ -0,0 +1,40 @@
+interface Index {
+    height: number,
+    weight: number
+}
+
+const parseArguments = (args: Array<string>): Index => {
+    if(args.length !== 4) throw new Error('We need 2 arguments only!')
+    if(!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            height: Number(args[2]),
+            weight: Number(args[3])
+        }
+    } else {
+        throw new Error('Provided values were not numbers')
+    }
+}
+
+const calculateBmi = (h: number, w: number) : string => {
+    const bmi = w/Math.pow(h/100, 2)
+    if(bmi < 18.5) {
+        return 'Underweight'
+    } else if (bmi >= 18.5 && bmi <=24.9) {
+        return 'Normal (healthy weight)'
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        return 'Overweight'
+    } else {
+        return 'Obese'
+    }
+}
+
+try {
+    const { height, weight } = parseArguments(process.argv)
+    console.log(calculateBmi(height, weight))
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if (error instanceof Error) {
+        errorMessage += ' ' + error.message
+    }
+    console.log(errorMessage)
+}
---
+++ part9/exerciseCalculator.ts
@@ -0,0 +1,86 @@
+interface Result {
+    periodLength: number;
+    trainingDays: number;
+    success: boolean;
+    rating: number;
+    ratingDescription: string;
+    target: number;
+    average: number;
+}
+
+interface RatingData {
+    point: number;
+    description: string;
+}
+
+interface ValueVariable {
+    hour: Array<number>;
+    target: number;
+}
+
+const parseArgument = (args: Array<string>, target: string): ValueVariable => {
+    let hourList: number[] = [];
+    if(args.length !== 7) throw new Error("An array must have 7 elements for 7 days.")
+    if((args.filter(hours => isNaN(Number(hours))).length == 0) && !isNaN(Number(target))) {
+        args.forEach(hours => hourList.push(Number(hours)))
+        return {
+            hour: hourList,
+            target: Number(target)
+        }
+    } else {
+        throw new Error('Provided values were not numbers!');
+    }
+}
+
+const rating = (arr: Array<number>, target: number): RatingData => {
+    if(arr.filter(hours => hours >= target).length >= 5) {
+        return {
+            point: 3,
+            description: "Nice job! Keep going!"
+        }
+    } else if (arr.filter(hours => hours >= target).length >= 3 && arr.filter(hours => hours >= target).length <= 4) {
+        return {
+            point: 2,
+            description: "Not too bad but could be better"
+        }
+    } else if (arr.filter(hours => hours >= target).length >= 1 && arr.filter(hours => hours >= target).length <= 2) {
+        return {
+            point: 1,
+            description: "You have to pay more effort!"
+        }
+    } else {
+        return {
+            point: 0,
+            description: "No exercise did much during last week!!!"
+        }
+    }
+}
+
+const calculateExercises = (arr: Array<number>, target: number) : Result => {
+    let sum = 0;
+    arr.forEach(hours => sum += hours);
+    return {
+        periodLength: arr.length,
+        trainingDays: arr.filter(hours => hours > 0).length,
+        success: (arr.filter(hours => hours >= target).length >= 3),
+        rating: rating(arr, target).point,
+        ratingDescription: rating(arr, target).description,
+        target: target,
+        average: sum/arr.length
+    }
+}
+
+try {
+    let list: string[] = [];
+    for(let i=3; i < process.argv.length; i++) {
+        list.push(process.argv[i]);
+    }
+    const { hour, target } = parseArgument(list, process.argv[2]);
+    console.log(calculateExercises(hour, target));
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if(error instanceof Error) {
+        errorMessage += ' ' + error.message
+    }
+    console.log(errorMessage)
+}
